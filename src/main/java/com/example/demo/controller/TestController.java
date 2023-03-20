3e1b48d1447d0ac7e862c839758affdf35bf9fda
fkffkfk <minisnake@naver.com>
2023-03-20 16:04:36 +0900

    3/20 수업완료

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test")
@@ -12,8 +15,36 @@ public class TestController {
     public String testController(){
      return "Hello world, dbsduddk";
  }
- @GetMapping("/{id}")
- public String testControllerWithPathVariables(@PathVariable(required = false)int id){
-    return "HEllo world ID: "+id;
+ @GetMapping("/testRequestParam")
+ public String testControllerWithRequestParam(@RequestParam(required = false)int id){
+    return "Hello world ID: "+id;
     }
-}
+
+    @GetMapping("/testRequestBody")
+    public String testControllerWithRequestParam(@RequestBody TestRequestBodyDTO testRequestBodyDTO){
+        return "Hello world,윤영아.ID: "+testRequestBodyDTO.getId()+".message:" +
+                testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody() {
+        List<String> list = new ArrayList<>();
+        list.add("csh");
+        list.add("csh2");
+        list.add("cat");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
+    }
+    @GetMapping("/testResponseEntity")
+    public ResponseEntity<?> testControllerResponseEntity() {
+        List<String> list = new ArrayList<>();
+        list.add("csh");
+        list.add("csh2");
+        list.add("cat");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+
+        //return ResponseEntity.ok().body(response);
+        return ResponseEntity.badRequest().body(response);
+    }
+    }
+
